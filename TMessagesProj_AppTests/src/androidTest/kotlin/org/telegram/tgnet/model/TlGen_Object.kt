1c875f780ea2e3db02fdb4cf8b82e24918ebda5f
djjskaIlIndjsikfjLehuao <i49185464@gmail.com>
2025-12-24 20:32:47 +0900

    Update TlGen_Object.kt

--- TMessagesProj_AppTests/src/androidTest/kotlin/org/telegram/tgnet/model/TlGen_Object.kt
+++ TMessagesProj_AppTests/src/androidTest/kotlin/org/telegram/tgnet/model/TlGen_Object.kt
@@ -1,6 +1,6 @@
-package org.telegram.tgnet.model
+package com.telix.messenger.tgnet.model
 
-import org.telegram.tgnet.OutputSerializedData
+import com.telix.messenger.tgnet.OutputSerializedData
 
 public interface TlGen_Object {
     fun serializeToStream(stream: OutputSerializedData)
